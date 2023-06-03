e5dd0a6fea90884123407f2d62a4942d9f0735f6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-02 21:18:30 -0300

    Minhas Frutas Preferidas

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ Console.WriteLine ("Minhas Frutas Preferidas");
+    Console.WriteLine ("1.Uva, 2.Laranja, 3.Pera, 4.Maçã, 5.Morango");
